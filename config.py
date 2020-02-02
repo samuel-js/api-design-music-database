7411808c01d807cea81bf14a449c9548da7a879c
Samuel E. Jiménez Sanabria <samuel.jimenez@sanabria.se>
2020-02-02 23:15:28 +0100

    Database details update

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
 import os
 SECRET_KEY = os.urandom(32)
-# Grabs the folder where the script runs.
+
+# Folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
+#----------------------------------------------------------------------------#
+# Database
+#----------------------------------------------------------------------------#
 
-# TODO IMPLEMENT DATABASE URL
 # SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost:5432/fyyur'
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:meIjmassjs@35.198.158.106:5432/fyyur'
+
+#Moved database to Cloud
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres@35.198.158.106:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
