28a7741f10555b420f5d0db9641302d12460bcd7
Samuel E. Jiménez Sanabria <samuel.jimenez@sanabria.se>
2020-02-02 13:20:05 +0100

    Pictuer display update

--- app.py
+++ app.py
@@ -217,9 +217,9 @@ def show_venue(venue_id):
     "phone": venue.phone,
     "website": venue.website,
     "facebook_link": venue.facebook_link,
-    "seeking_talent": venue.seeking_talent,
-    "seeking_description": venue.seeking_description,
     "image_link": venue.image_link,
+    "seeking_talent": venue.seeking_talent,
+    "seeking_description": venue.seeking_description, 
     "past_shows": past_shows,
     "upcoming_shows": upcoming_shows,
     "past_shows_count": len(past_shows),
@@ -227,7 +227,6 @@ def show_venue(venue_id):
   }
 
   return render_template('pages/show_venue.html', venue=venue_dict)
-
 
 
 #  Create Venue
@@ -247,7 +246,8 @@ def create_venue_submission():
     address=request.form['address'],
     phone=request.form['phone'],
     genres=request.form.getlist('genres'),
-    facebook_link=request.form['facebook_link']
+    facebook_link=request.form['facebook_link'],
+    image_link=request.form['image_link']
   )
 
   try:
@@ -358,7 +358,7 @@ def show_artist(artist_id):
       upcoming_shows.append(show_details)
 
 # Artist data to be displayed
-  data = {
+  artist_dict = {
       "id": artist.id,
       "name": artist.name,
       "genres": artist.genres,
@@ -367,16 +367,16 @@ def show_artist(artist_id):
       "phone": artist.phone,
       "website": artist.website,
       "facebook_link": artist.facebook_link,
-      "seeking_venue": artist.seeking_venue,
-      "seeking_description": artist.seeking_description,
       "image_link": artist.image_link,
+      "seeking_venue": artist.seeking_venue,
+      "seeking_description": artist.seeking_description, 
       "past_shows": past_shows,
       "upcoming_shows": upcoming_shows,
       "past_shows_count": len(past_shows),
       "upcoming_shows_count": len(upcoming_shows)
     }
 
-  return render_template('pages/show_artist.html', artist=data)
+  return render_template('pages/show_artist.html', artist=artist_dict)
 
 #  Update
 #  ----------------------------------------------------------------
@@ -393,6 +393,7 @@ def edit_artist(artist_id):
   form.state.process_data(artist.state)
   form.genres.process_data(artist.genres)
   form.facebook_link.process_data(artist.facebook_link)
+  form.image_link.process_data(artist.image_link)
 
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
@@ -406,6 +407,7 @@ def edit_artist_submission(artist_id):
       artist.phone = request.form['phone']
       artist.genres = request.form.getlist('genres')
       artist.facebook_link = request.form['facebook_link']
+      artist.image_link = request.form['image_link']
 
       db.session.commit()
       flash('Artist ' + request.form['name'] + ' was successfully updated!')
@@ -429,6 +431,7 @@ def edit_venue(venue_id):
   form.state.process_data(venue.state)
   form.genres.process_data(venue.genres)
   form.facebook_link.process_data(venue.facebook_link)
+  form.image_link.process_data(venue.image_link)
 
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
@@ -442,6 +445,7 @@ def edit_venue_submission(venue_id):
     venue.phone = request.form['phone']
     venue.genres = request.form.getlist('genres')
     venue.facebook_link = request.form['facebook_link']
+    venue.image_link = request.form['image_link']
 
     db.session.commit()
     flash('Venue ' + request.form['name'] + ' was successfully updated!')
@@ -470,8 +474,9 @@ def create_artist_submission():
       city=request.form['city'],
       state=request.form['state'],
       phone=request.form['phone'],
-      genres=request.form.getlist('genres'),
-      facebook_link=request.form['facebook_link']
+      genres=request.form.getlist['genres'],
+      facebook_link=request.form['facebook_link'],
+      image_link=request.form['image_link']
   )
 
   try:
@@ -479,15 +484,11 @@ def create_artist_submission():
     db.session.commit()
     flash('Artist ' + request.form['name'] + ' was successfully listed!')
 
-# TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
-
   except:
     flash('An error occurred. Artist ' + new_artist.name + ' could not be listed.', category='error')
     print('exc_info(): ', exc_info())
     db.session.rollback()
 
-# TODO: modify data to be the data object returned from db insertion
   finally:
     db.session.close()
     return redirect(url_for('artists'))
