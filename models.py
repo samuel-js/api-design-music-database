f53b5de80eed0038ec766f0d2d7115e2f334070e
Samuel E. Jiménez Sanabria <samuel.jimenez@sanabria.se>
2020-04-22 16:37:26 +0200

    minor changes

--- models.py
+++ models.py
@@ -14,7 +14,6 @@ def setup_db(app, database_path=database_path):
   app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
   db.app = app
   db.init_app(app)
-  
   migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
