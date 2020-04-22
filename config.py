13e8bacdeac9c7e6c74436607a7f83ef1c7107cf
Samuel E. Jiménez Sanabria <samuel.jimenez@sanabria.se>
2020-04-22 19:25:34 +0200

    database

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ DEBUG = True
 #----------------------------------------------------------------------------#
 
 
-#SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost:5432/wmd'
-SQLALCHEMY_DATABASE_URI = 'postgres://abaqafjqjpygxw:6278eb22c84c9488c044a2727ec4a530bd8bb6cee5cf100913c6380c21ce256b@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d2efim24lu0f1q'
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost:5432/wmd'
+#SQLALCHEMY_DATABASE_URI = 'postgres://abaqafjqjpygxw:6278eb22c84c9488c044a2727ec4a530bd8bb6cee5cf100913c6380c21ce256b@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d2efim24lu0f1q'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
